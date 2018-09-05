1b7f0455482ce8696c74c21a779472bd7bcd637e
fuchenxu2008 <fuchenxu2008@163.com>
2018-09-05 19:45:46 +0800

    new list

--- server/database.js
+++ server/database.js
@@ -21,30 +21,32 @@ module.exports = [{
   steps: [
     '1. You will need: <b> Your passport/Your apartment contract</b>',
     '2. Go to the <b> Police Station</b> at <b> Wenhui Plaza</b>: <br/>🚍 From <b> 西交大</b> bus station (the one in front of XJTLU), take bus No. <b> 2</b>, <b> 812</b>, <b> 142</b>, <b> 178</b> or <b> 228</b> to <b> 独墅湖高教区首末站</b>.',
-    '3. The Police Station looks like this:',
+    `3. The Police Station looks like this: <img src='https://ws4.sinaimg.cn/large/0069RVTdgy1fuyvif6v3hj30pw0k04b6.jpg' />`,
     '4. Go to the <b> Service Center</b> and you should obtain a document which looks like this:',
   ],
 }, {
   id: '2',
   title: 'Checking Your Residence Permit Expiry Date (International Students)',
-  note: 'It takes 2 to 4 weeks to renew your residence permit, and you can start the application for a new residence permit from 1 month before the expiry date. The latest you should start applying is 2 weeks before the expiry date. You will need to leave your passport at the registration office, and you can ask for a document to temporarily replace your passport. (Travelling within China also needs a passport).',
+  note: `It takes 2 to 4 weeks to renew your residence permit, and you can start the application for a new residence permit from 1 month before the expiry date. 
+The latest you should start applying is 2 weeks before the expiry date. 
+You will need to leave your passport at the Exit-Entry Bureau, and you can ask for a receipt（外国人签证证件受理回执）to temporarily replace your passport. (Travelling within China also needs 2 documents: the receipt and a copy of your passport).`,
   image: 'https://www.barclayscorporate.com/content/corppublic/en/insight-and-research/industry-expertise/social-housing-trends/_jcr_content/bc-content-center-par-1/textimageeditor/image.corppublic.full.high.jpg',
   steps: [
     '1. Take your <b> passport</b> and look at your <b> residence permit’s expiry date</b>:',
     `2. <b> You will need to prepare</b>:
       <br/>⭐️ Registration form of temporary residence issued by the local police station
       <br/>⭐️ Passport
-      <br/>⭐️ Copies of passport, front page, latest visa/ residence page and the last entry stamp
+      <br/>⭐️ Copies of passport, front page, latest visa/residence page and the last entry stamp
       <br/>⭐️ 1 passport photo (2 inches, white background)
-      <br/>⭐️ Application fee (+ 400元), in Chinese debit or credit cards only, NO CASH.`,
+      <br/>⭐️ Application fee (400-800元), in Chinese debit or credit cards only, NO CASH.`,
     `3. <b> You need to ask from XJTLU Global</b>:
       <br/>⭐️ Completed residence permit application form
       <br/>⭐️ Statement from the university
       <br/>⭐️ Copies of XJTLU’s license certificate`,
       `4. <b> Go to the Exit-Entry Administration of SIP Public Security Branch（园区公安局出入境管理大队</b>.
       <br/>🏫 Address: 苏州工业园区万盛街55号园区工商大厦一楼（园区一站式务中心）
-      <br/>⏰ Mon to Fri, 9:00-11:45, 14:00-17:15
-      <br/>🚍 By bus: no. 2, 6, 16, 47, 100, 106, 108, 158, 116, 166, 168, 219, 228, 258, 812 to 园区行政中心（西）or 旺墩路万盛街`,
+      <br/>⏰ Mon to Fri, 9:00-11:45, 14:00-16:30
+      <br/>🚍 By bus: no. 2, 16, 100, 106, 228, 812 to 园区行政中心（西）or 旺墩路万盛街`,
       '5. Follow the procedures',
       '6. Wait for an <b> e-mail</b> from XJTLU Global which says that your passport is ready for pick up',
   ],
@@ -68,26 +70,32 @@ module.exports = [{
   id: '4',
   title: 'Paying Your Apartment Rent And Fees',
   image: 'http://classamgmt.com/wp-content/uploads/2014/07/Property_Management_Rent_Payment.png',
-  note: 'Don’t forget to take into account possible electricity and water fees.',
+  note: 'Don’t forget to take into account possible electricity, heating and water fees.',
   steps: [
-    '1. Go to your apartment’s front desk and ask how much you owe them. Ask for detailed calculations: how much is the rent, how much is the electricity and water fees.',
+    '1. Go to your apartment’s front desk and ask how much you owe them. Ask for detailed calculations: how much is the rent, how much is the electricity, heating and water fees.',
     '2. Go to the bank to withdraw some cash to pay the rent (if needed)',
-    '3. Pay your dues',
+    '3. Pay your dues & <b> keep all the receipts.</b>',
   ],
 }, {
   id: '5',
-  title: 'Getting Your Phone Number & Wi-Fi (From China Unicom)',
+  title: 'Opening a Chinese bank account',
   image: 'https://us-east-1.tchyn.io/snopes-production/uploads/2017/09/no_cellular_signal_fb.jpg?resize=865%2C452',
   steps: [
     `1. <b> You will need:</b>
-    <br/>⭐️ Your passport WITH the student visa
+    <br/>⭐️ Your passport
     <br/>⭐️ Admission notice/ student ID card
     <br/>⭐️ Chinese mobile phone number
-    <br/>⭐️ Accommodation registration form (link)
+    <br/>⭐️ Accommodation registration form
     <br/>⭐️ A little cash (+ 100元)`,
-    `2. Go to the ICBC near the school:
-    (for students from Iran, Russia, Syria, Sudan, and Ukraine please refer to info here)`,
-    '3. Tell the lady/ guard next to the ticket machine that you want to open a bank account.',
+    `2. The ICBC near XJTLU is no longer available. It is recommended to go to:
+      <br/>🏦 <b> Bank of Communications</b>
+      <br/>📍 江苏省苏州市林泉街598号翰林邻里中心1楼 1st Floor Hanlin Neighborhood Center, No. 598 Linquan Street
+      <br/>📞 Phone: 051262768127
+      <hr />
+      <br/>🏦 China CITIC Bank (students from Iran, Russia, Syria, Sudan, and Ukraine are recommended to go to CITIC)
+      <br/>📍 江苏省苏州市工业园月亮湾路15号 No. 15 Moon Bay Road
+      <br/>📞 Phone: 051262817041`,
+    '3. Tell the lady/guard next to the ticket machine that you want to open a bank account.',
     `4. If they don’t understand English, show them this:
     <br/>🗣 我要开一个账户
     Wŏ yào kāi yī ge zhànghù (I want to open a bank account)
@@ -115,41 +123,67 @@ module.exports = [{
   ],
 }, {
   id: '7',
-  title: 'Opening A Chinese Bank Account: For Students From Iran, Russia, Syria, Sudan, and Ukraine',
+  title: 'Accommodation/ Apartment Check-In',
   image: 'https://www.justaskpoland.com/wp-content/uploads/2017/04/The-best-banks-in-Poland.jpeg',
-  note: 'This is mandatory and needs to be done within 24 hours of your arrival. But if you arrive on a Sunday, the police station is closed so you can take a day off and come on Monday.',
+  note: '',
   steps: [
-    `1. <b> You will need:</b>
-    <br/>⭐️ Your passport
-    <br/>⭐️ Your apartment contract`,
-    `2. Go to the Police Station at Wenhui Plaza:
-    <br/>🚍 From 西交大 bus station (the one in front of XJTLU), take bus no. 2, 812, 142, 178 or 228 to 独墅湖高教区首末站.`,
-    '3. The Police Station looks like this:',
-    '4. Go to the <b> Service Center</b> and you should obtain a document which looks like this:',
+    `1. You will need: 
+      <br />⭐️ Your passport
+      <br />⭐️ Money for deposit`,
+      '2. Go to the apartment’s front desk and ask to do the check-in.',
+      '3. Sign the contract and ask for a copy',
+      '4. Ask for the check-in form'
   ],
 }, {
   id: '8',
-  title: 'Applying For Residence Permit (International Students)',
-  image: 'https://www.sdu.dk/-/media/images/information_til/international_staff/topbilleder/workandresidencepermitcolourbox3611792.jpg?h=330&la=en&w=770&hash=DA5F7D081C88B1338B26AC4B799BF14D5AF06531',
+  title: 'Health check',
+  image: 'https://www.justaskpoland.com/wp-content/uploads/2017/04/The-best-banks-in-Poland.jpeg',
+  note: 'Warning: Please keep in mind that you will have to hand in your passport for this activity. Do not eat any food in the morning.',
   steps: [
-    `1. <b> You will need to prepare:</b>
-    <br/>⭐️ Registration form of temporary residence issued by the local police station
-    <br/>⭐️ Passport
-    <br/>⭐️ Copies of passport, front page, latest visa/ residence page and the last entry stamp
-    <br/>⭐️ The offer letter
-    <br/>⭐️ Health check report (link)
-    <br/>⭐️ ID photos (must be taken at the exit-entry office for 20元)
-    <br/>⭐️ Money: Application fee (+ 400元), in Chinese debit or credit cards only. And 20元 for photo taking.`,
-    `2. <b> You need to ask from XJTLU Global:</b>
-    <br/>⭐️ Completed residence permit application form
-    <br/>⭐️ JW202
-    <br/>⭐️ Statement from the university
-    <br/>⭐️ Copies of XJTLU’s license certificate (老三张)`,
-    `3. <b> Go to the Exit-Entry Administration of SIP Public Security Branch（园区公安局出入境管理大队）.</b>
-    <br/>🏫 Address: 苏州工业园区万盛街55号园区工商大厦一楼（园区一站式务中心）
-    <br/>⏰ Mon to Fri, 9:00-11:45, 14:00-17:15
-    <br/>🚍 By bus: no. 2, 6, 16, 47, 100, 106, 108, 158, 116, 166, 168, 219, 228, 258, 812 to 园区行政中心（西）or 旺墩路万盛街`,
-    '4. Follow the procedures, don’t forget to get your <b> photos</b> taken first!',
-    '5. Wait for an <b> e-mail</b> from XJTLU Global which says that your passport is ready for pick up',
+    `1. You will need: 
+      <br />⭐️ Your passport
+      <br />⭐️ 2 passport-sized photos`,
+      '2. Take the free bus to the health check center. It leaves at 06:15 from the mentioned places.',
+      '3. Did you miss the bus? Then take a taxi to: 新区国际旅行卫生保健中心. It will cost you around 80元。',
+      '4. Follow the procedures and wait for further notice.'
+  ],
+}, {
+  id: '9',
+  title: 'Getting your phone number & Wi-Fi (from China Unicom)',
+  image: 'https://www.justaskpoland.com/wp-content/uploads/2017/04/The-best-banks-in-Poland.jpeg',
+  steps: [
+    `1. You will need:
+<br />⭐️ Your passport
+<br />⭐️ Money to pay for the SIM card (100元) and/ or Wi-Fi & router.`,
+'2. From XJTLU’s cube building, find the big road and walk to the right, around 400m, and you’ll find Wenxing Plaza (文星广场).',
+'3. At Wenxing Plaza, find this yellow restaurant, follow the road, and turn left. The mobile phone shop is called China Unicom and has an orange-colored shop:',
+'4. Ask for a SIM card and/ or Wi-Fi & router. For Wi-Fi, keep in mind that 8 Mbps (megabit/second) = 1 MBps (megabyte/second).'
+  ],
+}, {
+  id: '10',
+  title: 'Applying for Residence Permit (international students)',
+  image: 'https://www.justaskpoland.com/wp-content/uploads/2017/04/The-best-banks-in-Poland.jpeg',
+  note: `Your X1 Visa must be converted to a residence permit within 30 days from date of entry, which you can see from your entry stamp:
+Missing this point will lead to a 500元 fine per day (with a maximum of 10,000元).
+You will need to leave your passport at the Exit-Entry Bureau, and you can ask for a receipt （外国人签证证件受理回执）to temporarily replace your passport. (Travelling within China also needs 2 documents: the receipt and a copy of your passport).`,
+  steps: [
+    `1. You will need to prepare:
+<br />⭐️ Accommodation Registration Form issued by the local police station
+<br />⭐️ Passport
+<br />⭐️ Copies of passport, front page, latest visa/ residence page and the last entry stamp
+<br />⭐️ The offer letter
+<br />⭐️ Health check report
+<br />⭐️ ID photos (must be taken at the exit-entry bureau for 20元)
+<br />⭐️ Money: Application fee (400-800元), in Chinese debit or credit cards only, NO CASH. And 20元 for photo taking.`,
+`2. You need to ask from XJTLU Global:
+<br />⭐️ Residence permit application form
+<br />⭐️ JW202
+<br />⭐️ Statement from the university
+<br />⭐️ XJTLU’s license certificate copy`,
+`3. Go to the Exit-Entry Bureau of SIP Public Security Branch （园区公安局出入境管理大队）. 
+<br />📍 Address: 苏州工业园区万盛街55号园区工商大厦一楼（园区一站式务中心）
+<br />⏰ Mon to Fri, 9:00-11:45, 14:00-16:30
+<br />🚍 By bus: no. 2, 16, 100, 228, 812 to 园区行政中心（西）or 旺墩路万盛街`,
+'4. Follow the procedures, don’t forget to get your photos taken first!'
   ],
 }];
